0454dde8b98d0018f2f4a80f95fbcc022f06bd0e
abedin99 <pjoy813@gmail.com>
2025-02-18 14:54:00 +0600

    Frontend homepage added & admin added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Specify the callback that should be used to generate the redirect path.
+        Authenticate::redirectUsing(function (Request $request) {
+            if (!$request->expectsJson() && $request->routeIs('admin.*')) {
+                return route('admin.login');
+            }
+
+            if (!$request->expectsJson() && $request->user('web')) {
+                return route('login');
+            }
+        });
+
+        // The callback that should be used to generate the authentication redirect path.
+        RedirectIfAuthenticated::redirectUsing(function (Request $request) {
+            if ($request->user('admin') && $request->routeIs('admin.*')) {
+                return route('admin.dashboard');
+            }
+
+            if ($request->user('web')) {
+                return route('dashboard');
+            }
+        });
     }
 }
